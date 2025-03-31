98d67436b9983eb1e59bb37f9304fbf2db0eb37f
jasonnugat <jb.pozzi@gmail.com>
2025-03-31 13:33:48 +0200

    needs to be done

--- Frontend/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { AuthGuard } from './core/auth.guard';
-
-export const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: 'login', loadChildren: () => import('./features/auth/auth.module').then(m => m.AuthModule) },
-  { path: 'home', loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule), canActivate: [AuthGuard] }
-];
